cbfb8625559a9950e72ea9f6747435bf7e2ee17b
Dori Crabbs <doricrabbs@gmail.com>
2020-08-09 21:11:13 +1000

    Basic audio player and sound effect placeholder

---
+++ Assets/Prototype2/Scripts/AudioPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource player;
+    
+    public static AudioPlayer Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void PlaySoundEffect(
+        AudioClip clip
+        )
+    {
+        if(player == null || clip == null)
+        {
+            return;
+        }
+        player.PlayOneShot(clip);
+    }
+}
---
+++ Assets/Prototype2/Scripts/SoundImpactPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundImpactPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip impactSound;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(AudioPlayer.Instance == null || impactSound == null)
+        {
+            return;
+        }
+        AudioPlayer.Instance.PlaySoundEffect(impactSound);
+    }
+}
