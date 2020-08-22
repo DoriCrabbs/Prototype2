ed1623631cf526ee2ac1189cdbb0a984c2a0ace2
Dori Crabbs <doricrabbs@gmail.com>
2020-08-22 22:26:33 +1000

    Reward system elements

---
+++ Assets/Prototype2/Scripts/GlowingBall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingBall : MonoBehaviour
+{
+    //public void OnCollisionEnter(Collision collision)
+    //{
+    //    GameSceneManager.Instance.AddGlowingBall();
+    //    GameObject.Destroy(gameObject);
+    //}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        GameSceneManager.Instance.AddGlowingBall();
+        GameObject.Destroy(gameObject);
+    }
+}
