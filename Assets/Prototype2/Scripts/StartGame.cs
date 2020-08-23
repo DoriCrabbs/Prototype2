10da640287495e403fc415124681e7ff6f483a73
Dori Crabbs <doricrabbs@gmail.com>
2020-08-23 21:12:40 +1000

    Add intro to the game

---
+++ Assets/Prototype2/Scripts/StartGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void StartTutorial()
+    {
+        SceneManager.LoadSceneAsync("Level_Tutorial");
+        this.gameObject.SetActive(false);
+    }
+}
