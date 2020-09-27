a283ddeafc5872f040bf4c28e2c9b7c349a0d8af
Dori Crabbs <doricrabbs@gmail.com>
2020-09-27 18:33:51 +1000

    Add main menu

---
+++ Assets/Prototype2/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void ContinueGame()
+    {
+        SceneManager.LoadSceneAsync("Intro");
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadSceneAsync("Intro");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit game");
+        Application.Quit();
+    }
+}
