f550257a936a0f2d1ca0dffff8b1e4b480e472ee
Dori Crabbs <doricrabbs@gmail.com>
2020-11-23 00:39:50 +1000

    Emissive material

---
+++ Assets/Prototype2/Scripts/MushinDelay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushinDelay : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mushin;
+
+    [SerializeField]
+    private float delay = 5f;
+
+    void Start()
+    {
+        StartCoroutine(ShowMushinWithDelay());
+    }
+
+    private IEnumerator ShowMushinWithDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        if(mushin != null)
+        {
+            mushin.SetActive(true);
+        }
+    }
+}
