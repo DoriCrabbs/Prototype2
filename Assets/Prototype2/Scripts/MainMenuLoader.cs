953b0a06d5fb14cd8c886a85ec0bbb9ba4d0a0d8
Dori Crabbs <doricrabbs@gmail.com>
2020-11-23 20:13:21 +1000

    Splash screen

---
+++ Assets/Prototype2/Scripts/MainMenuLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLoader : MonoBehaviour
+{
+    [SerializeField]
+    private float delay;
+
+    void Start()
+    {
+        StartCoroutine(LoadMainMenuDelay());
+    }
+
+    private IEnumerator LoadMainMenuDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadSceneAsync("Main_Menu");
+    }
+}
